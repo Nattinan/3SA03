4b3f1a1cc42eec9bbda0f41622bb89e995cb3dda
Nattinan <nattinan001@gmail.com>
2018-09-29 14:53:11 +0700

    GameLogic

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class CharacterCard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             active: false,
         }
     }
-    
+
     activate = () => {
-        if(!this.state.active){
-            this.setState({active: true})
+        if (!this.state.active) {
+            this.setState({ active: true })
         }
     }
 
-    render(){
-        let className = `card ${this.state.active ? 'activeCard': ''}`
+    activate = () => {
+        if (!this.state.active) {
+            this.props.activationHandler(this.props.value)
+            this.setState({ active: true })
+        }
+    }
+    activationHandler = c => { console.log(`${c} has been activated.`) }
+    render() {
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
             <div className={className} onClick={this.activate}>
                 {this.props.value}
